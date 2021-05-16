844cb80aca29db32cde2390309fa36f17f524be2
Lettuce <qkqkfo@gmail.com>
2021-05-16 20:43:30 +0900

    Time: 4964 ms (41.05%), Space: 31.9 MB (47.69%) - LeetHub

--- minimum-knight-moves/minimum-knight-moves.py
+++ minimum-knight-moves/minimum-knight-moves.py
@@ -2,27 +2,36 @@ from collections import deque
 
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
+        if x == 0 and y == 0: return 0
         dr = (2, 2, -2, -2, 1, 1, -1, -1)
         dc = (-1, 1, 1, -1, 2, -2, 2, -2)
         que = deque()
-        visit = [[None for _ in range(601)] for __ in range(601)]
-        
-        sr, sc = 300, 300
+        SIZE = 601
+        mat = [[None for _ in range(SIZE)] for __ in range(SIZE)]
+        sr, sc = SIZE//2, SIZE//2 
         x += sr
         y += sc
-        que.append((sr, sc ,0))
-        visit[sr][sc] = True
+        from_start = (sr, sc, 0, True)
+        from_target = (x, y, 0, False)
+        que.append(from_start)
+        que.append(from_target)
+        mat[sr][sc] = from_start
+        mat[x][y] = from_target
         
         while len(que) > 0:
-            r, c, cnt = que.popleft()
-            if r == x and c == y: return cnt
+            node = que.popleft()
+            r, c, cnt, is_from_start = node
             for i in range(len(dr)):
                 nr = r + dr[i]
                 nc = c + dc[i]
-                if nr < 0 or nr >= 601 or nc < 0 or nc >= 601: continue
-                if visit[nr][nc]: continue
-                visit[nr][nc] = True
-                que.append((nr, nc, cnt+1))
+                if nr < 0 or nr >= SIZE or nc < 0 or nc >= SIZE: continue
+                next_node = (nr, nc, cnt + 1, is_from_start)
+                if not mat[nr][nc]:
+                    mat[nr][nc] = next_node
+                    que.append(next_node)
+                    continue
+                elif mat[nr][nc][3] != is_from_start:
+                    return mat[nr][nc][2] + cnt + 1
                 
         return -1
     
