000d3f57dadc45b251f4607699f53d69c29f81e6
Lettuce <qkqkfo@gmail.com>
2021-05-29 02:58:53 +0900

    Time: 424 ms (51.89%), Space: 28.6 MB (81.75%) - LeetHub

--- divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.py
+++ divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.py
@@ -7,11 +7,12 @@ class Solution:
         nums.sort()
         
         for num in nums:
-            if not num in counter or counter[num] == 0: continue
-            for i in range(k):
-                next_num = num + i
-                if not next_num in counter or counter[next_num] == 0: return False
+            if not num in counter: continue
+            for next_num in range(num, num + k):
+                if not next_num in counter: return False
                 counter[next_num] -= 1
+                if counter[next_num] == 0:
+                    counter.pop(next_num)
         return True
             
                 
