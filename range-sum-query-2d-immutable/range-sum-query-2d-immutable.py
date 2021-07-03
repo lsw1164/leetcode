19b7f6bb95524e2a884366b2f361e4acaf9e54d8
Lettuce <qkqkfo@gmail.com>
2021-07-04 00:57:38 +0900

    Time: 596 ms (36.91%), Space: 24.1 MB (18.89%) - LeetHub

--- range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -3,37 +3,25 @@ class NumMatrix:
     def __init__(self, matrix: List[List[int]]):
         R = len(matrix)
         C = len(matrix[0])
-        
-        self.h_cumul = [[0 for _ in range(C)] for __ in range(R)]
-        self.v_cumul = [[0 for _ in range(C)] for __ in range(R)]
-        
-        for r in range(R):
-            cumul = 0
-            for c in range(C):
-                cumul += matrix[r][c]
-                self.h_cumul[r][c] = cumul
-                
-        for c in range(C):
-            cumul = 0
-            for r in range(R):
-                cumul += matrix[r][c]
-                self.v_cumul[r][c] = cumul
+        cache = [[0 for _ in range(C)] for __ in range(R)]
+        cache[0][0] = matrix[0][0]
+        for r in range(1, R):
+            cache[r][0] = cache[r-1][0] + matrix[r][0]
+        for c in range(1, C):
+            cache[0][c] = cache[0][c-1] + matrix[0][c]
+        for r in range(1, R):
+            for c in range(1, C):
+                cache[r][c] = matrix[r][c] + cache[r][c-1] + cache[r-1][c] - cache[r-1][c-1]
+        self.cache = cache
 
+        
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        dr, dc = row2 - row1, col2 - col1
-        sum_region = 0
-        if dr > dc:
-            for c in range(col1, col2+1):
-                sum_region += self.v_cumul[row2][c]
-                if row1 > 0: sum_region -= self.v_cumul[row1-1][c]
-        else:
-            for r in range(row1, row2+1):
-                sum_region += self.h_cumul[r][col2]
-                if col1 > 0: sum_region -= self.h_cumul[r][col1-1]
-                    
-        return sum_region
+        region = self.cache[row2][col2]
+        if col1 > 0: region -= self.cache[row2][col1-1]
+        if row1 > 0: region -= self.cache[row1-1][col2]
+        if col1 > 0 and row1 > 0: region += self.cache[row1-1][col1-1]
+        return region
         
-
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
