3d3f2a8abd793ebbdadb8d0a77b191d3aac3e5af
Lettuce <qkqkfo@gmail.com>
2021-07-09 02:41:15 +0900

    Time: 152 ms (12.72%), Space: 46.3 MB (56.77%) - LeetHub

--- merge-two-binary-trees/merge-two-binary-trees.js
+++ merge-two-binary-trees/merge-two-binary-trees.js
@@ -12,17 +12,19 @@
  * @return {TreeNode}
  */
 
-function dfs(node1, node2, mergedNode) {
-    mergedNode.val = (node1?.val ?? 0) + (node2?.val ?? 0);
-    
-    if(node1?.left || node2?.left) {
-        mergedNode.left = new TreeNode(); 
-        dfs(node1?.left, node2?.left, mergedNode.left);
+function dfs(node1, node2) {
+    node1.val += node2.val;
+    if(node1.left && node2.left) {
+        dfs(node1.left, node2.left);
     }
-    
-    if(node1?.right || node2?.right) {
-        mergedNode.right = new TreeNode(); 
-        dfs(node1?.right, node2?.right, mergedNode.right);
+    if(node1.right && node2.right) {
+        dfs(node1.right, node2.right);
+    }
+    if(!node1.left && node2.left) {
+        node1.left = node2.left;
+    }
+    if(!node1.right && node2.right) {
+        node1.right = node2.right;
     }
 }
 
@@ -34,7 +36,6 @@ var mergeTrees = function(root1, root2) {
     if(root2 === null) {
         return root1;
     }
-    const mergedRoot = new TreeNode();
-    dfs(root1, root2, mergedRoot);
-    return mergedRoot;
+    dfs(root1, root2);
+    return root1;
 };
