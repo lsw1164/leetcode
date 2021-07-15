8e92778e3d9ea6f4160c96870db84a9c132ce243
Lettuce <qkqkfo@gmail.com>
2021-07-15 19:12:31 +0900

    Time: 88 ms (65.64%), Space: 41.2 MB (15.81%) - LeetHub

--- move-zeroes/move-zeroes.ts
+++ move-zeroes/move-zeroes.ts
@@ -16,7 +16,7 @@ function moveZeroes(nums: number[]): void {
     const isValidIdx = (idx) => idx >= 0 && idx < nums.length; 
     let slow = nums.indexOf(0, 0)
     let fast = findNonZeroIdx(nums, slow);
-    while(isValidIdx(slow) && isValidIdx(fast) && slow <= fast) {
+    while(isValidIdx(slow) && isValidIdx(fast)) {
         [nums[slow], nums[fast]] = [nums[fast], nums[slow]];
         slow = nums.indexOf(0, slow);
         fast = findNonZeroIdx(nums, fast);
