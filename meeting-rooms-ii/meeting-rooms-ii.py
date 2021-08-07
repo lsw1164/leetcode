03a02a3f59d23d99e3d07fe4d382b03bfc7c4e03
Lettuce <qkqkfo@gmail.com>
2021-08-08 01:00:35 +0900

    Time: 111 ms (11.61%), Space: 17.4 MB (66.42%) - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.py
+++ meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,20 +1,19 @@
 import heapq
 
 class Solution:
+    
+    def peek(self, pq):
+        peek = heapq.heappop(pq)
+        heapq.heappush(pq, peek)
+        return peek
+    
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         intervals.sort()
-        
         pq = []
+        
         for [start, end] in intervals:
-            if len(pq) == 0:
-                heapq.heappush(pq, end)
-                continue
-            available_time = heapq.heappop(pq)
-            heapq.heappush(pq, available_time)
-            
-            if available_time <= start:
+            if len(pq) > 0 and self.peek(pq) <= start:
                 heapq.heappop(pq)
-                
             heapq.heappush(pq, end)
             
         return len(pq)
