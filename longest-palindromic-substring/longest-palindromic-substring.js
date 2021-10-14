f75228e6b3e06fa72001732ed02f9b49d93ae6f4
Lettuce <qkqkfo@gmail.com>
2021-10-14 23:01:07 +0900

    Time: 1306 ms (22.28%), Space: 84.2 MB (6.47%) - LeetHub

---
+++ longest-palindromic-substring/longest-palindromic-substring.js
@@ -0,0 +1,35 @@
+
+
+var longestPalindrome = function(s) {
+    
+    const memo = [...Array(s.length)].map(() => Array(s.length).fill(null));
+    
+    for(let len = s.length; len > 0; len--) {
+        for(let left = 0; left + len - 1 < s.length; left++) {
+            const right = left + len - 1;
+            if(isPalindrome(s, left, right)) {
+                return s.substring(left, right+1);
+            }
+        }
+    }
+    console.log(memo)
+    
+    function isPalindrome(str, left, right) {
+        if(left < 0 || left >= str.length || right < 0 || right >= str.length) {
+            return false;
+        }
+        if(memo[left][right] !== null) {
+            return memo[left][right];
+        }
+        
+        const len = right - left + 1;
+        if(len <= 2) {
+            memo[left][right] = str[left] === str[right];
+        } else {
+            memo[left][right] = str[left] === str[right] && isPalindrome(str, left + 1, right - 1);
+        }
+        
+        return memo[left][right];
+    } 
+    
+};
