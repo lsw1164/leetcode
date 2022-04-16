adf13f093b57241d84385c6f99d0f1ffd3371e82
Lettuce <qkqkfo@gmail.com>
2022-04-16 18:47:43 +0900

    Time: 64 ms (89.03%), Space: 43.6 MB (75.08%) - LeetHub

--- 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.js
+++ 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.js
@@ -1,32 +1,29 @@
 var flatten = function(head) {
-    flattenRecursive(head);
+    let prevNode = null;
+    let curNode = null;
+    
+    function dfs(node) {
+        while(node) {
+            prevNode = curNode;
+            curNode = node;
+            const nextNode = curNode.next;
+            
+            if(prevNode) {
+                prevNode.next = curNode;
+                prevNode.child = null;
+            }
+            curNode.prev = prevNode;
+            
+            if(node.child) {
+                dfs(node.child);
+            }
+            node = nextNode;
+        }
+    }
+    
+    dfs(head);
+    
     return head;
 };
 
-function flattenRecursive(node) {
-    let cur = node;
-    
-    while(cur) {
-        const nextNode = cur.next;
-        const childNode = cur.child;
 
-        if(childNode) {
-            cur.next = childNode;
-            childNode.prev = cur;
-            const lastChild = flattenRecursive(childNode);
-            lastChild.next = nextNode;
-            if(nextNode) {
-                nextNode.prev = lastChild;
-            }
-            cur.child = null
-        }
-        cur = nextNode;
-    }
-    
-    let lastNode = node;
-    while(node) {
-        lastNode = node;
-        node = node.next;
-    }
-    return lastNode;
-}
